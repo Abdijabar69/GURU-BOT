b44867950c0218dc5852a5973352e9a97de521f2
Abdijabar69 <154331572+Abdijabar69@users.noreply.github.com>
2023-12-19 15:29:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['252 67 6021717', 'king bot', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
